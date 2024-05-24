9f1ebf1bf3b4c57329155c45af586d9ca7563c14
breadbread1984 <breadbread1984@163.com>
2024-05-24 17:45:42 +0800

    update

--- agent.py
+++ agent.py
@@ -17,7 +17,7 @@ class Agent(object):
       tokenizer, llm = CodeLlama(locally)
     else:
       raise Exception('unknown model!')
-    tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663') + [load_knowledge_graph(password = '19841124'), load_database('bs_challenge_financial_14b_dataset/dataset/博金杯比赛数据.db')]
+    tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663') + [load_knowledge_graph(password = '19841124', locally = True), load_database('bs_challenge_financial_14b_dataset/dataset/博金杯比赛数据.db', locally = True)]
     prompt = agent_template(tokenizer, tools)
     llm = llm.bind(stop = ["<|eot_id|>"])
     chain = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"])} | prompt | llm | ReActJsonSingleInputOutputParser()
