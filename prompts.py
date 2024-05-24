546e279a849ee8aa30e9397a1d9d879798f188d3
breadbread1984 <breadbread1984@163.com>
2024-05-24 14:10:58 +0800

    update

--- prompts.py
+++ prompts.py
@@ -179,3 +179,28 @@ def triplets_qa_template(tokenizer, triplets):
   template = PromptTemplate(template = prompt, input_variables = ['question'])
   return template
 
+def sqlite_prompt(tokenizer):
+  _sqlite_prompt = """You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use date('now') function to get the current date, if the question involves "today".
+
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+"""
+  PROMPT_SUFFIX = """Only use the following tables:
+{table_info}
+
+Question: {input}"""
+  messages = [
+    ('role': 'user', 'content': _sqlite_prompt + PROMPT_SUFFIX)
+  ]
+  prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
+  template = PromptTemplate(template = prompt, input_variables = ['input', 'table_info', 'top_k'])
+  return template
