dd4e00fa9c0cc1b9d21207db66e4c66cd5b9ae13
breadbread1984 <breadbread1984@163.com>
2024-06-20 09:26:58 +0800

    update

--- tools.py
+++ tools.py
@@ -20,7 +20,7 @@ from langchain_experimental.sql import SQLDatabaseChain
 from prompts import entity_generation_template, triplets_qa_template, sqlite_prompt, condense_template, rag_template
 from models import Llama3, CodeLlama, Qwen2
 
-def load_vectordb(host = "bolt://localhost:7687", username = "neo4j", password = None, database = 'neo4j', locally = False):
+def load_vectordb(host = "bolt://localhost:7687", username = "neo4j", password = None, database = 'neo4j', locally = False, tokenizer = None, llm = None):
   class ProspectusInput(BaseModel):
     query: str = Field(description = "招股说明书相关的问题")
     user_id: str = Field(description = '用户编号')
@@ -181,7 +181,6 @@ def load_vectordb(host = "bolt://localhost:7687", username = "neo4j", password =
     """,
     index_name = "summary"
   )
-  tokenizer, llm = Llama3(locally = locally)
 
   return ProspectusTool(config = ProspectusConfig(
     neo4j = neo4j,
@@ -196,7 +195,7 @@ def load_vectordb(host = "bolt://localhost:7687", username = "neo4j", password =
     llm = llm
   ))
 
-def load_knowledge_graph(host = 'bolt://localhost:7687', username = 'neo4j', password = None, database = 'neo4j', locally = False):
+def load_knowledge_graph(host = 'bolt://localhost:7687', username = 'neo4j', password = None, database = 'neo4j', locally = False, tokenizer = None, llm = None):
   class ProspectusInput(BaseModel):
     query: str = Field(description = "招股说明书相关的问题")
 
@@ -245,10 +244,9 @@ def load_knowledge_graph(host = 'bolt://localhost:7687', username = 'neo4j', pas
       raise NotImplementedError("Prospectus does not suppert async!")
 
   neo4j = Neo4jGraph(url = host, username = username, password = password, database = database)
-  tokenizer, llm = Llama3(locally = locally)
   return ProspectusTool(config = ProspectusConfig(neo4j = neo4j, tokenizer = tokenizer, llm = llm))
 
-def load_database(sqlite_path, locally = False):
+def load_database(sqlite_path, locally = False, tokenizer = None, llm = None):
   class DatabaseInput(BaseModel):
     query: str = Field(description = "需要询问的金融问题")
 
@@ -272,7 +270,6 @@ def load_database(sqlite_path, locally = False):
     async def _arun(self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
       raise NotImplementedError("DatabaseTool does not support async!")
 
-  tokenizer, llm = Qwen2(locally)
   db = SQLDatabase.from_uri('sqlite:///%s' % sqlite_path)
   return DatabaseTool(config = DatabaseConfig(db = db, tokenizer = tokenizer, llm = llm))
 
